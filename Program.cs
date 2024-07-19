0332a7322bea50dbb4a242b82e115892598dac5f
Mracka Michal <mracka@gmail.com>
2024-07-19 18:51:51 +0200

    v1.2.0 - update metadata

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Windows.Forms;
 
 namespace MOBIeditor {
@@ -9,7 +10,22 @@ namespace MOBIeditor {
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main() {
+		static int Main() {
+			
+			if (!OperatingSystem.IsWindows())
+			{
+				Console.WriteLine(@"This program is only supported on Windows.");
+				return 1;
+			}
+
+			WindowsOnlyMethod();
+			return 0;
+		}
+
+		[SupportedOSPlatform("windows")]
+		static void WindowsOnlyMethod()
+		{
+			// Windows-specific code here
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
